e1bb2ae73708f20f824ac9b1e04620e716ab6b19
Xavier Sala <utrescu@gmail.com>
2018-08-11 18:03:52 +0200

    Create DbTest to Helper

--- tests/unit/ColorsRepositoryTest.cs
+++ tests/unit/ColorsRepositoryTest.cs
@@ -18,23 +18,11 @@ namespace colorsRest.Tests.UnitTests
         public ColorsRepositoryTests()
         {
             var dades = Helper.TestColors;
-            colorsSetMock = CreateDbSetMock(dades);
+            colorsSetMock = Helper.CreateDbSetMock(dades);
             colorsContextMock = new Mock<ColorsContext>();
             colorsContextMock.Setup(x => x.Colors).Returns(colorsSetMock.Object as DbSet<Color>);
         }
 
-        private static Mock<DbSet<T>> CreateDbSetMock<T>(IEnumerable<T> elements) where T : class
-        {
-            var elementsAsQueryable = elements.AsQueryable();
-            var dbSetMock = new Mock<DbSet<T>>();
-
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elementsAsQueryable.Provider);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elementsAsQueryable.Expression);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elementsAsQueryable.ElementType);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(elementsAsQueryable.GetEnumerator());
-
-            return dbSetMock;
-        }
 
         [Fact]
         public void UnnecessariTestForGetAllColors()
--- tests/unit/Helper.cs
+++ tests/unit/Helper.cs
@@ -1,6 +1,9 @@
 
 using System.Collections.Generic;
+using System.Linq;
 using colorsRest.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace colorsRest.Tests.UnitTests
 {
@@ -37,6 +40,19 @@ namespace colorsRest.Tests.UnitTests
 
                 return colors;
             }
+        }
+
+        public static Mock<DbSet<T>> CreateDbSetMock<T>(IEnumerable<T> elements) where T : class
+        {
+            var elementsAsQueryable = elements.AsQueryable();
+            var dbSetMock = new Mock<DbSet<T>>();
+
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elementsAsQueryable.Provider);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elementsAsQueryable.Expression);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elementsAsQueryable.ElementType);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(elementsAsQueryable.GetEnumerator());
+
+            return dbSetMock;
         }
     }
 }
